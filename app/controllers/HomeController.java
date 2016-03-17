0312c8edfc5462223cd25b319544971e3f8e8fd8
Steven Nguyen <snguyen@oakland.edu>
2016-03-17 12:29:54 -0400

    added a navigation bar with some styling adjustments, need to work on formatting to fit page better

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -11,7 +11,7 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
-    public  Result Registration() {
+    public  Result registration() {
         return ok(registration.render("registration page"));
     }
 
@@ -23,8 +23,8 @@ public class HomeController extends Controller {
         return ok(about.render("about"));
     }
 
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
+    public Result home() {
+        return ok(home.render("Your new application is ready."));
     }
 
 }
